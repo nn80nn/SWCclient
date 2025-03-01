003fc8053bf7859ac6d709e6f632680ad9b0b632
Nikita <nn80nn80nn80nn80nn@gmail.com>
2025-03-01 10:40:20 -0400

    Create git

---
+++ src/main/java/nvi/server/swcclient/SwCclientApplication.java
@@ -0,0 +1,13 @@
+package nvi.server.swcclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwCclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwCclientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nvi/server/swcclient/SwCclientApplicationTests.java
@@ -0,0 +1,13 @@
+package nvi.server.swcclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwCclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
